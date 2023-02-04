ea6d5195b8dd406ce1ee2abe418ae81f93296128
Roberto Gomes <rjrg.snf20@uea.edu.br>
2023-02-03 22:42:52 -0400

    Correções

--- Assets/Scripts/Defensores/Bullet.cs
+++ Assets/Scripts/Defensores/Bullet.cs
@@ -9,16 +9,30 @@ public class Bullet : MonoBehaviour
     private float speed;
     private float damage;
 
+    private Vector3 lastPos;
+
     public void SetBullet(Transform target, float speed, float damage)
     {
         this.target = target;
         this.speed = speed;
         this.damage = damage;
+        Destroy(gameObject, 0.7f);
     }
     
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+            lastPos = transform.position;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+        
     }
 
     private void OnTriggerEnter2D(Collider2D col)
