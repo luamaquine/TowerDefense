e5a3c5b4a9335dd7b75506b2bc3a686323b000ba
arthursantosuea <aldsb.snf20@uea.edu.br>
2023-02-05 11:41:17 -0400

    final changes

--- Assets/Scripts/FadeCall.cs
+++ Assets/Scripts/FadeCall.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Image = UnityEngine.UI.Image;
@@ -9,9 +9,11 @@ public class FadeCall : MonoBehaviour
    public Image blackfade;
    public Animator animFade;
    private string lvlName;
+   public GameObject optionsPanel;
    
    public void Fade(string scene)
    {
+      Debug.Log("Clicou");
       lvlName = scene;
       StartCoroutine(GoToNextLevel());
    }
@@ -22,6 +24,27 @@ public class FadeCall : MonoBehaviour
       yield return new WaitUntil(() => blackfade.color.a == 1);
       SceneManager.LoadScene(lvlName);
    }
-    
+
+   public void Restart()
+   {
+      SceneManager.LoadScene("Test-Eduardo");
+      CloseOptionsPanel();
+   }
+
+   public void OpenOptionsPanel()
+   {
+      Time.timeScale = 0;
+      optionsPanel.SetActive(true);
+   }
+   public void CloseOptionsPanel()
+   {
+      Time.timeScale = 1;
+      optionsPanel.SetActive(false);
+   }
+   public void Quit()
+   {
+      
+      Application.Quit();
+   }
     
 }
