b26685a641997fe603568f2abdb6b1f21b74d9d3
Luã Maury Maquiné da Silva <83655458+luamaquine@users.noreply.github.com>
2023-02-07 11:03:10 -0400

    teste

---
+++ Assets/HordeManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Generic.List;
+
+public class HordeManager : MonoBehaviour
+{
+    public List<GameObject> hordePrefabs; // Lista de prefabs de inimigos para cada horda
+    public List<int> hordeEnemies; // Lista de números de inimigos para cada horda
+
+    private int currentHorde = 0; // Índice da horda atual
+    private bool hordeActive = false; // Variável para verificar se a horda está ativa
+    public Transform spawnPoint; // Transform do spawn point
+    public EnemyLife enemyLife; // Script de vida do inimigo
+
+    void Start()
+    {
+        StartHorde(); // Inicia a primeira horda ao iniciar o jogo
+    }
+
+    void Update()
+    {
+        // Verifica se a horda está ativa
+        if (hordeActive)
+        {
+            int enemiesAlive = 0;
+
+            // Conta o número de inimigos vivos
+            foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+            {
+                if (enemy.GetComponent<enemyLife>().isAlive)
+                {
+                    enemiesAlive++;
+                }
+            }
+
+            // Verifica se todos os inimigos foram derrotados
+            if (enemiesAlive == 0)
+            {
+                hordeActive = false;
+                currentHorde++;
+
+                // Verifica se ainda há hordas restantes
+                if (currentHorde < hordePrefabs.Count)
+                {
+                    StartHorde();
+                }
+            }
+        }
+    }
+
+    void StartHorde()
+    {
+        hordeActive = true;
+
+        // Instancia a horda atual no spawn point
+        for (int i = 0; i < hordeEnemies[currentHorde]; i++)
+        {
+            Instantiate(hordePrefabs[currentHorde], spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
